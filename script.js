1c095613b39679b6af766f0fd88eda1e3e73158a
jasmin <jasminchakma895@gmail.com>
2023-02-21 21:27:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var typed = new Typed(".writer",{
+    strings: [ "HI I'M JASMIN", "I AM A Web Designer...", " I AM A Graphic Designer...", "I AM A Animator...", "I AM A Digital Marketer...", " HI I'M JASMIN"],
+    typeSpeed: 50,
+    backSpeed: 50,
+});
